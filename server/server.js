d446c0768598a4e2325ac8d100f8c2e99f72e4c4
Shishir Oli <srish821@gmail.com>
2025-11-07 07:58:05 +0545

    Cors fix

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
   "http://localhost:5173", // dev
-  "https://mernn-blog.netlify.app/", // production frontend
+  "https://mernn-blog.netlify.app", // production frontend
 ];
 
 app.use(cors({
